ba7665901f9503885c5754d03c5aaaf4b6b02433
Eric <ericdedmonds@gmail.com>
2025-04-15 14:09:28 -0400

    Created the background and Panel for game.

---
+++ pgame.py
@@ -0,0 +1,45 @@
+import pygame
+
+from Pygame_Project.Battle.battle_tut1 import panel_img
+
+pygame.init()
+
+# game window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 400
+
+# CREATE GAME WINDOW
+screen = pygame.display .set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Change_name_ofGAME')
+
+clock = pygame.time.Clock()
+FPS = 60
+
+#load images
+background_img = pygame.image.load('img/Background/background.png').convert_alpha()
+panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
+
+# function for drawing background
+
+def draw_bg():
+    screen.blit(background_img, (0,0))
+
+def draw_panel():
+    screen.blit(panel_img, (0,0))
+
+#game loop
+run = True
+while run:
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    draw_bg()
+    draw_panel()
+    pygame.display.flip()
+
+pygame.quit()
