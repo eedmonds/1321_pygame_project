2dd22c5afe5324e292e9e3dce08c260706442cbc
Eric <ericdedmonds@gmail.com>
2025-04-16 14:19:04 -0400

    For Charactor.py fixed spelling errors added animation list, action frame_index and updated time variable and functionality to animate the frames. I updated the  update function. Verified that all is working. For the pygame.py file . I updated the draw_text  and draw_panel to handle to show stats for charactors. added font to game for displaying HP and charactor name on the bottom panel.

--- Charactor.py
+++ Charactor.py
@@ -6,14 +6,45 @@ class Charactor():
         self.name = name
         self.max_hp = max_hp
         self.hp = max_hp
-        self.strenght = strength
+        self.strength = strength
         self.start_potions = potions
         self.potions = potions
         self.alive = True
-        img = pygame.image.load(f'img/{self.name}/Idle/0.png')
-        self.image = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+        self.animation_list =[]
+        self.action = 1 # 0: idle, 1:attack, 2:hurt, 3: death
+        self.frame_index = 0
+        self.updat_time = pygame.time.get_ticks()
+        #load idle images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+
+        # load attack images
+        temp_list = []
+        for i in range(8):
+            img = pygame.image.load(f'img/{self.name}/Attack/{i}.png')
+            img = pygame.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+    def update(self):
+        animation_cooldown  = 100
+        #handle animation
+        #update image
+        self.image = self.animation_list[self.action][self.frame_index] # check if enough time has passed before updating
+        if pygame.time.get_ticks() - self.updat_time > animation_cooldown:
+            self.updat_time = pygame.time.get_ticks()
+
+            self.frame_index += 1
+            #if the amination has index out of bound set frame_index = 0.
+            if self.frame_index >= len(self.animation_list[self.action]):
+                self.frame_index = 0
+
 
     def draw(self,function):
         function.blit(self.image, self.rect)
